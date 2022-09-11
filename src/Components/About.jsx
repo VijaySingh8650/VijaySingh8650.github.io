db5f2a68478ec36138f03b947118bb610f568a7c
Vijay_singh <rathorevijay995@gmail.com>
2022-09-11 13:43:36 +0530

    portfolio

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 import styles from "./about.module.css";
 export default function About() {
   return (
-    <Box id="about">
+    <Box id="about" className={styles.container}>
           <Text variant={["sm", "base", "md"]} color="#127369" className={styles.about}>About</Text>
-          <hr />
+          <Text className={styles.name}  variant={["sm", "base", "md"]}>I am Vijay Singh Rathore</Text>
+          <Text className={styles.aboutdetails}>Skilled web developer with some projects of experience in website development through MERN stack technology with willingness to enhance skill and  knowledge as a web developer.</Text>
     </Box>
   )
 }
