b0eb31f85e62611c6813e34c80909dc45a05816b
Vijay_singh <rathorevijay995@gmail.com>
2022-10-23 08:28:03 +0530

    change

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -81,11 +81,11 @@ export default function Skills() {
           <Text className={styles.title} mb="1rem">GitHub Stats</Text>
 
                 
-                <Image align="center"  src="https://github-readme-stats.vercel.app/api/top-langs/?username=VijaySingh8650&hide=html,css&title_color=ffffff&text_color=c9cacc&icon_color=4AB197&bg_color=1A2B34" width="25%"/>
+                <Image align="center"  src="https://github-readme-stats.vercel.app/api/top-langs/?username=VijaySingh8650&hide=html,css&title_color=ffffff&text_color=c9cacc&icon_color=4AB197&bg_color=1A2B34" width="35%"/>
                
 
                 
-                <Image align="center"  src="https://github-readme-stats.vercel.app/api?username=VijaySingh8650&show_icons=true&line_height=27&count_private=true&title_color=ffffff&text_color=c9cacc&icon_color=4AB097&bg_color=1A2B34" alt="Martin's GitHub Stats" width="40%" />
+                <Image align="center"  src="https://github-readme-stats.vercel.app/api?username=VijaySingh8650&show_icons=true&line_height=27&count_private=true&title_color=ffffff&text_color=c9cacc&icon_color=4AB097&bg_color=1A2B34" alt="Martin's GitHub Stats" width="50%" />
                
 
               </Box>
