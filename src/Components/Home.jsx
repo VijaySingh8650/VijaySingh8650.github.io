8ef7a9ec65a2b36c604b29811e138604b2ff216b
Vijay_singh <rathorevijay995@gmail.com>
2022-10-23 08:34:06 +0530

    change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,7 +9,7 @@ let style = {
 const Home = () => {
    const onButtonClick = () => {
         // using Java Script method to get PDF file
-        fetch('Resume.pdf').then(response => {
+        fetch('Vijay-Singh-Rathore-Resume.pdf').then(response => {
             response.blob().then(blob => {
                 // Creating new object of PDF file
                 const fileURL = window.URL.createObjectURL(blob);
