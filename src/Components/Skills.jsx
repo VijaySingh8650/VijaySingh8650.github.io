149e58398a7c8f806690bd1e227c0d52a0ad423c
VijaySingh8650 <rathorevijay995@gmail.com>
2023-12-21 21:16:42 +0530

    updated resume

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,4 +1,4 @@
-import { Box, SimpleGrid, Text } from '@chakra-ui/react'
+import { Box, Image, SimpleGrid, Text } from '@chakra-ui/react'
 import React from 'react'
 import styles from "./skills.module.css";
 import { SiHtml5, SiCss3, SiJavascript, SiReact, SiNextdotjs, SiExpress, SiMongodb, SiTypescript  } from 'react-icons/si';
@@ -6,7 +6,8 @@ import { FaNodeJs } from "react-icons/fa";
 import { MdLeaderboard } from "react-icons/md";
 import { RiTeamFill} from "react-icons/ri";
 import { GoGitCompare} from "react-icons/go";
-
+import Static from './Static';
+import GitHub from './Git';
 
 export default function Skills() {
   return (
@@ -74,7 +75,23 @@ export default function Skills() {
                    </Box>
                   
               </Box>
-          </SimpleGrid>         
+          </SimpleGrid>
+          {/* <Static />
+          <br/>
+          <GitHub /> */}
+          {/* <Box mt="3rem" display="flex" flexDirection={"column"} alignItems="center"> */}
+              
+          {/* <Text className={styles.title} mb="1rem">GitHub Stats</Text> */}
+
+                
+                {/* <Image align="center"  src="https://github-readme-stats.vercel.app/api/top-langs/?username=VijaySingh8650&hide=html,css&title_color=ffffff&text_color=c9cacc&icon_color=4AB197&bg_color=1A2B34" width="35%"/> */}
+               
+
+                
+                {/* <Image align="center"  src="http://github-readme-streak-stats.herokuapp.com?user=VijaySingh8650&theme=dark&background=1A2B34" alt="Vijay's GitHub Stats" width="40%" /> */}
+               
+
+          {/* </Box> */}
     </Box>
   )
 }
